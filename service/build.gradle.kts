a42ad3fa807b375bff54c2d4f1014b4b1adb56c4
suzhanlee <wlscww@kakao.com>
2026-01-01 15:57:12 +0900

    config: Docker Compose와 Application YAML 설정 추가
    - MySQL 8.0 Docker Compose 구성 추가
    - service 모듈 application.yml 추가 (datasource, JPA, Flyway 설정)
    - service 모듈 build.gradle.kts에 spring-data-jpa, infra:flyway 의존성 추가
    - ServiceApplication에 컴포넌트 스캔 설정 추가

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -7,9 +7,11 @@ dependencies {
     implementation(project(":activity"))
     implementation(project(":common"))
     implementation(project(":infra:database"))
+    implementation(project(":infra:flyway"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- service/src/main/java/com/leesuchan/service/ServiceApplication.java
+++ service/src/main/java/com/leesuchan/service/ServiceApplication.java
@@ -2,8 +2,25 @@ package com.leesuchan.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.leesuchan.service",
+    "com.leesuchan.account",
+    "com.leesuchan.activity",
+    "com.leesuchan.common",
+    "com.leesuchan.infra"
+})
+@EnableJpaRepositories(basePackages = {
+    "com.leesuchan.infra.database"
+})
+@EntityScan(basePackages = {
+    "com.leesuchan.account.domain.model",
+    "com.leesuchan.activity.domain.model"
+})
 public class ServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceApplication.class, args);
